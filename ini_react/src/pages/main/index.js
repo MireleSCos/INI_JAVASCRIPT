434623971430e46716804faa4e3016e331edebcc
MireleSCos <mirele.si.co@outlook.com>
2020-03-15 01:56:21 -0300

    React, movimento de pages add

--- ini_react/src/pages/main/index.js
+++ ini_react/src/pages/main/index.js
@@ -16,21 +16,44 @@ export default class Main extends Component {
     jogar no front*/
 
     state = {
-        products: []
+        products: [],
+        productInfo: {},
+        page: 1, //Faz referencia a pagina que o sistema está atualmente
     };
 
     componentDidMount(){  //Executa assim que o componente for montado em tela
         this.loadProducts();
     }  
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
         console.log(response.data.docs);
+        const { docs, ...productInfo} = response.data;
 
-        this.setState({products : response.data.docs}); //Atribuindo os valores ao estado
+        this.setState({products : docs,productInfo, page}); //Atribuindo os valores ao estado
     };
+    prevPage = () => {
+        const{page, productInfo} = this.state;
+
+        if (page === 1) return
+
+        const prevPage = page -1;
+
+        this.loadProducts(prevPage); // Pegar as informações da proxima pagina 
+
+    }
+    nextPage = () => {
+        const{page, productInfo} = this.state;
+
+        if(page === productInfo.pages) return;
+        
+        const nextPage = page +1;
+
+        this.loadProducts(nextPage); // Pegar as informações da proxima pagina 
+    }
+
     render(){
-        const {products} = this.state;
+        const {products, productInfo, page} = this.state;
         return (            
             <div >        
                 <h6>Quandidade de produtos em estoque : {this.state.products.length}</h6>
@@ -43,6 +66,10 @@ export default class Main extends Component {
                             <a  href="">Acessar</a>
                         </article>
                     ))}
+                    <div className="actions">
+                        <button disabled ={page === 1} onClick={this.prevPage}>Anterior</button>
+                        <button disabled ={page === productInfo.pages} onClick={this.nextPage}>Proximo</button>
+                    </div>
                     
                 </div>
 
